7f739f39521fe1491ef7a66cc66e94dd6b5d66da
zhang long <135420055+handsomelong922@users.noreply.github.com>
2026-03-28 18:28:38 +0800

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -133,6 +133,17 @@ class AppConfig:
 				waf_cookie_names=None,
 				check_in_method='POST',
 			),
+			'zhj': ProviderConfig(
+				name='zhj',
+				domain='https://api.zhenhaoji.qzz.io',
+				login_path='/console/personal',
+				sign_in_path='/api/user/checkin',
+				user_info_path='/api/user/self',
+				api_user_key='new-api-user',
+				bypass_method=None,
+				waf_cookie_names=None,
+				check_in_method='POST',
+			),
 		}
 
 		# 尝试从环境变量加载自定义 providers
